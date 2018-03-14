8b908072945afe37a8ff077f4b839cbb92f631cf
sandeepdj <sandeepdj1988@gmail.com>
2018-03-14 17:17:18 +0530

    menulist

--- src/app/modules/sidebar/sidebar.component.ts
+++ src/app/modules/sidebar/sidebar.component.ts
@@ -6,10 +6,144 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
-
+   
   constructor() { }
 
   ngOnInit() {
   }
+
+
+ menusList:any[]=[
+	{
+		"group": "Navigation",
+		"Parent": [
+			{
+				"name": "Dashboard",
+				"children": [
+					{
+						"name": "DEFAULT"
+					},
+					{
+						"name": "E-Commerce"
+					},
+					{
+						"name": "Analytics"
+					}
+				]
+			},
+			{
+				"name": "Widgets",
+				"children": [
+					{
+						"name": "Statistics"
+					},
+					{
+						"name": "Data"
+					},
+					{
+						"name": "Chart"
+					}
+				]
+			}
+		]
+	},
+	{
+		"group": "UI Elelemts",
+		"Parent": [
+			{
+				"name": "Basic",
+				"children": [
+					{
+						"name": "Alert"
+					},
+					{
+						"name": "Button"
+					},
+					{
+						"name": "Accordians"
+					},
+					{
+						"name": "Tabs"
+					},
+					{
+						"name": "Lists"
+					}
+				]
+			},
+			{
+				"name": "Advance",
+				"children": [
+					{
+						"name": "Modal"
+					},
+					{
+						"name": "Notification"
+					},
+					{
+						"name": "Sliders"
+					}
+				]
+			},
+			{
+				"name": "Animations",
+				"children": []
+			}
+		]
+	},
+	{
+		"group": "Forms",
+		"Parent": [
+			{
+				"name": "Form",
+				"children": [
+					{
+						"name": "Components"
+					},
+					{
+						"name": "Advance"
+					},
+					{
+						"name": "Validation"
+					}
+				]
+			},
+			{
+				"name": "Form Picker",
+				"children": []
+			},
+			{
+				"name": "Form Select",
+				"children": []
+			}
+		]
+	},
+	{
+		"group": "Tables",
+		"Parent": [
+			{
+				"name": "Bootstrap Tables",
+				"children": [
+					{
+						"name": "Basic Table"
+					},
+					{
+						"name": "Border Table"
+					}
+				]
+			},
+			{
+				"name": "Data Tables",
+				"children": [
+					{
+						"name": "Basic Table"
+					},
+					{
+						"name": "Paginig Table"
+					}
+				]
+			}
+		]
+	}
+];
 
 }
